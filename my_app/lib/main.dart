4cff412217b8dcbfb226e655274875ae4761bd4b
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2024-02-22 17:27:40 +0700

    testing done

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,175 +1,534 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Validate());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Validate extends StatelessWidget {
+  const Validate({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CouterPage(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Validation Site')),
+        body: const Center(
+          child: Validation(),
+        ),
+      ),
     );
   }
 }
 
-class CouterPage extends StatefulWidget {
-  const CouterPage({super.key});
+class Validation extends StatefulWidget {
+  const Validation({super.key});
   @override
   State<StatefulWidget> createState() {
-    return _CouterPageState();
+    return _ValidationState();
   }
 }
 
-class _CouterPageState extends State<CouterPage> {
-  final MyObject myObject = MyObject(3);
-  final TextEditingController _newval = TextEditingController();
-  String value = '';
-  int m = 0;
+class _ValidationState extends State<Validation> {
+  int _step = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.cyanAccent, title: const Text('CouterPage')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+    return Stepper(
+      currentStep: _step,
+      onStepCancel: () {
+        if (_step > 0) {
+          setState(() {
+            _step -= 1;
+          });
+        }
+      },
+      onStepContinue: () {
+        if (_step <= 0) {
+          setState(() {
+            _step += 1;
+          });
+        }
+      },
+      onStepTapped: (int index) {
+        setState(() {
+          _step = index;
+        });
+      },
+      steps: const <Step>[
+        Step(title: Text('B1'), content: FirstForm()),
+      ],
+    );
+  }
+}
+
+class FirstForm extends StatefulWidget {
+  const FirstForm({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FirstFormState();
+  }
+}
+
+class _FirstFormState extends State<FirstForm> {
+  final _formKey = GlobalKey<FormState>();
+  var provinceList;
+  var districtList;
+  var wardList;
+  Future<void> loadLocationData() async {
+    try {
+      String data =
+          await rootBundle.loadString('assets/data/don_vi_hanh_chinh.json');
+      Map<String, dynamic> jsonData = json.decode(data);
+
+      List provinceData = jsonData["province"];
+      provinceList =
+          provinceData.map((json) => Province.fromMap(json)).toList();
+      print(provinceList[0]);
+
+      List districtData = jsonData["district"];
+      districtList =
+          districtData.map((json) => District.fromMap(json)).toList();
+      List wardData = jsonData["ward"];
+      wardList = wardData.map((json) => Ward.fromMap(json)).toList();
+    } catch (e) {
+      debugPrint('Lỗi load dữ liệu vị trí: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String dropdownValue = '';
+    loadLocationData();
+    return Form(
+      key: _formKey,
+      child: Column(
         children: [
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          //   child: FutureBuilder<void>(
+          //       future: loadLocationData(),
+          //       builder: (BuildContext context, AsyncSnapshot snapshot) {
+          //         return DropdownButtonFormField<String>(
+          //             value: dropdownValue,
+          //             icon: const Icon(Icons.arrow_downward),
+          //             items: provinceData.map((json) {
+          //               DropdownMenuItem(
+          //                 value: provinceData["id"],
+          //                 child: Text(provinceData['name']),
+          //               );
+          //             }).toList(),
+          //             onChanged: (String? value) {
+          //               setState(() {
+          //                 dropdownValue = value!;
+          //               });
+          //             });
+          //       }),
+          // ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 16),
-            child: TextField(
-              controller: _newval,
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextFormField(
               keyboardType: TextInputType.number,
-              decoration: const InputDecoration(hintText: 'Nhập số nguyên tố n'),
-              onChanged: (String value) {
-                m = int.parse(value);
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                //Cho bàn phím chỉ hiện số
+                LengthLimitingTextInputFormatter(10)
+                //Giới hạn số kí tự nhập là 10
+              ],
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Enter your phone number',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Vui lòng điền vào chỗ trống";
+                }
+                return null;
               },
             ),
-          ),
-          const SizedBox(width: 5),
-          OutlinedButton.icon(
-            onPressed: () {
-              String newvalue = myObject.textpower(m).toString();
-              print(myObject.textpower(m));
-              final updatevalue = newvalue;
-              _newval.value = _newval.value.copyWith(
-                text: updatevalue,
-                selection: TextSelection.collapsed(offset: updatevalue.length),
-              );
-              m = myObject.textpower(m);
-            },
-            icon: const Icon(Icons.power),
-            label: const Text('Tính lũy thừa bậc n'),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Giá trị hiện tại: ${myObject.value}'),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  OutlinedButton.icon(
-                    onPressed: () {
-                      myObject.decrease();
-                      setState(() {});
-                    },
-                    icon: const Icon(Icons.remove),
-                    label: const Text('Giảm'),
-                  ),
-                  const SizedBox(width: 5),
-                  OutlinedButton.icon(
-                    onPressed: () {
-                      myObject.increase();
-                      setState(() {});
-                    },
-                    icon: const Icon(Icons.add),
-                    label: const Text('Tăng'),
-                  ),
-                  const SizedBox(width: 5),
-                  OutlinedButton.icon(
-                    onPressed: () {
-                      myObject.square();
-                      setState(() {});
-                    },
-                    icon: const Icon(Icons.square),
-                    label: const Text('Lũy thừa bậc 2'),
-                  ),
-                  const SizedBox(width: 5),
-                  OutlinedButton.icon(
-                    onPressed: () {
-                      myObject.power(3);
-                      setState(() {});
-                    },
-                    icon: const Icon(Icons.power),
-                    label: const Text('Lũy thừa bậc 3'),
-                  ),
-                  const SizedBox(width: 5),
-                  OutlinedButton.icon(
-                    onPressed: () {
-                      myObject.reset();
-                      setState(() {});
-                    },
-                    icon: const Icon(Icons.reset_tv),
-                    label: const Text('Đặt lại'),
-                  ),
-                ],
-              ),
-            ],
-          ),
+          )
         ],
       ),
     );
   }
 }
 
-class MyObject {
-  int _value;
-  MyObject(this._value);
+class Province {
+  String? id;
+  String? name;
+  String? level;
+  Province({
+    this.id,
+    this.name,
+    this.level,
+  });
 
-  int get value => _value;
-  set value(newValue) => _value = newValue;
-
-  void increase() {
-    _value++;
+  Province copyWith({
+    String? id,
+    String? name,
+    String? level,
+  }) {
+    return Province(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      level: level ?? this.level,
+    );
   }
 
-  void decrease() {
-    _value--;
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'level': level,
+    };
   }
 
-  void square() {
-    _value = _value * _value;
+  factory Province.fromMap(Map<String, dynamic> map) {
+    return Province(
+      id: map['id'] != null ? map['id'] as String : null,
+      name: map['name'] != null ? map['name'] as String : null,
+      level: map['level'] != null ? map['level'] as String : null,
+    );
   }
 
-  void reset() {
-    _value = 0;
+  String toJson() => json.encode(toMap());
+
+  factory Province.fromJson(String source) =>
+      Province.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Province(id: $id, name: $name, level: $level)';
+
+  @override
+  bool operator ==(covariant Province other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id && other.name == name && other.level == level;
   }
 
-  int power(int n) {
-    int base = _value;
-    if (n == 0) {
-      _value = 1;
-    } else {
-      for (int i = 2; i <= n; i++) {
-        _value = _value * base;
-      }
-    }
-    return _value;
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode ^ level.hashCode;
+}
+
+class District {
+  String? id;
+  String? name;
+  String? level;
+  String? provinceId;
+  District({
+    this.id,
+    this.name,
+    this.level,
+    this.provinceId,
+  });
+
+  District copyWith({
+    String? id,
+    String? name,
+    String? level,
+    String? provinceId,
+  }) {
+    return District(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      level: level ?? this.level,
+      provinceId: provinceId ?? this.provinceId,
+    );
   }
 
-  int textpower(int m) {
-    int base = m;
-    int result = 0;
-    if (m == 0) {
-      m = 1;
-    } else {
-      for (int i = 2; i <= base; i++) {
-        m = m * base;
-      }
-      result = m;
-    }
-    m = 0;
-    return result;
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'level': level,
+      'provinceId': provinceId,
+    };
+  }
+
+  factory District.fromMap(Map<String, dynamic> map) {
+    return District(
+      id: map['id'] != null ? map['id'] as String : null,
+      name: map['name'] != null ? map['name'] as String : null,
+      level: map['level'] != null ? map['level'] as String : null,
+      provinceId:
+          map['provinceId'] != null ? map['provinceId'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory District.fromJson(String source) =>
+      District.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'District(id: $id, name: $name, level: $level, provinceId: $provinceId)';
+  }
+
+  @override
+  bool operator ==(covariant District other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.level == level &&
+        other.provinceId == provinceId;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^ name.hashCode ^ level.hashCode ^ provinceId.hashCode;
+  }
+}
+
+class Ward {
+  String? id;
+  String? name;
+  String? level;
+  String? districtId;
+  String? provinceId;
+  Ward({
+    this.id,
+    this.name,
+    this.level,
+    this.districtId,
+    this.provinceId,
+  });
+
+  Ward copyWith({
+    String? id,
+    String? name,
+    String? level,
+    String? districtId,
+    String? provinceId,
+  }) {
+    return Ward(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      level: level ?? this.level,
+      districtId: districtId ?? this.districtId,
+      provinceId: provinceId ?? this.provinceId,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'level': level,
+      'districtId': districtId,
+      'provinceId': provinceId,
+    };
+  }
+
+  factory Ward.fromMap(Map<String, dynamic> map) {
+    return Ward(
+      id: map['id'] != null ? map['id'] as String : null,
+      name: map['name'] != null ? map['name'] as String : null,
+      level: map['level'] != null ? map['level'] as String : null,
+      districtId:
+          map['districtId'] != null ? map['districtId'] as String : null,
+      provinceId:
+          map['provinceId'] != null ? map['provinceId'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Ward.fromJson(String source) =>
+      Ward.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Ward(id: $id, name: $name, level: $level, districtId: $districtId, provinceId: $provinceId)';
+  }
+
+  @override
+  bool operator ==(covariant Ward other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.level == level &&
+        other.districtId == districtId &&
+        other.provinceId == provinceId;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        level.hashCode ^
+        districtId.hashCode ^
+        provinceId.hashCode;
+  }
+}
+
+class AddressInfo {
+  Province? province;
+  District? district;
+  Ward? ward;
+  String? street;
+  AddressInfo({
+    this.province,
+    this.district,
+    this.ward,
+    this.street,
+  });
+
+  AddressInfo copyWith({
+    Province? province,
+    District? district,
+    Ward? ward,
+    String? street,
+  }) {
+    return AddressInfo(
+      province: province ?? this.province,
+      district: district ?? this.district,
+      ward: ward ?? this.ward,
+      street: street ?? this.street,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'province': province?.toMap(),
+      'district': district?.toMap(),
+      'ward': ward?.toMap(),
+      'street': street,
+    };
+  }
+
+  factory AddressInfo.fromMap(Map<String, dynamic> map) {
+    return AddressInfo(
+      province: map['province'] != null
+          ? Province.fromMap(map['province'] as Map<String, dynamic>)
+          : null,
+      district: map['district'] != null
+          ? District.fromMap(map['district'] as Map<String, dynamic>)
+          : null,
+      ward: map['ward'] != null
+          ? Ward.fromMap(map['ward'] as Map<String, dynamic>)
+          : null,
+      street: map['street'] != null ? map['street'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AddressInfo.fromJson(String source) =>
+      AddressInfo.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'AddressInfo(province: $province, district: $district, ward: $ward, street: $street)';
+  }
+
+  @override
+  bool operator ==(covariant AddressInfo other) {
+    if (identical(this, other)) return true;
+
+    return other.province == province &&
+        other.district == district &&
+        other.ward == ward &&
+        other.street == street;
+  }
+
+  @override
+  int get hashCode {
+    return province.hashCode ^
+        district.hashCode ^
+        ward.hashCode ^
+        street.hashCode;
+  }
+}
+
+class UserInfo {
+  String? name;
+  String? email;
+  String? phoneNumber;
+  DateTime? birthDate;
+  AddressInfo? address;
+  UserInfo({
+    this.name,
+    this.email,
+    this.phoneNumber,
+    this.birthDate,
+    this.address,
+  });
+
+  UserInfo copyWith({
+    String? name,
+    String? email,
+    String? phoneNumber,
+    DateTime? birthDate,
+    AddressInfo? address,
+  }) {
+    return UserInfo(
+      name: name ?? this.name,
+      email: email ?? this.email,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      birthDate: birthDate ?? this.birthDate,
+      address: address ?? this.address,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'email': email,
+      'phoneNumber': phoneNumber,
+      'birthDate': birthDate?.millisecondsSinceEpoch,
+      'address': address?.toMap(),
+    };
+  }
+
+  factory UserInfo.fromMap(Map<String, dynamic> map) {
+    return UserInfo(
+      name: map['name'] != null ? map['name'] as String : null,
+      email: map['email'] != null ? map['email'] as String : null,
+      phoneNumber:
+          map['phoneNumber'] != null ? map['phoneNumber'] as String : null,
+      birthDate: map['birthDate'] != null
+          ? DateTime.fromMillisecondsSinceEpoch(map['birthDate'] as int)
+          : null,
+      address: map['address'] != null
+          ? AddressInfo.fromMap(map['address'] as Map<String, dynamic>)
+          : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserInfo.fromJson(String source) =>
+      UserInfo.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'UserInfo(name: $name, email: $email, phoneNumber: $phoneNumber, birthDate: $birthDate, address: $address)';
+  }
+
+  @override
+  bool operator ==(covariant UserInfo other) {
+    if (identical(this, other)) return true;
+
+    return other.name == name &&
+        other.email == email &&
+        other.phoneNumber == phoneNumber &&
+        other.birthDate == birthDate &&
+        other.address == address;
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+        email.hashCode ^
+        phoneNumber.hashCode ^
+        birthDate.hashCode ^
+        address.hashCode;
   }
 }
--- my_app/test/widget_test.dart
+++ my_app/test/widget_test.dart
@@ -1,30 +1,121 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'dart:convert';
+import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:my_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('Test constructor return object with right given data:', () {
+    var province = Province(
+        id: '01', name: "Thành phố Hà Nội", level: 'Thành phố trung ương');
+    var district = District(
+        id: '924', provinceId: '92', name: "Huyện Vĩnh Thạnh", level: 'Huyện');
+    var ward = Ward(
+        id: '00001',
+        provinceId: '01',
+        districtId: '001',
+        name: "Phường Phúc Xá",
+        level: 'Phường');
+    var addressInfo = AddressInfo(
+        province: province,
+        district: district,
+        ward: ward,
+        street: "Đường Bắc Từ Liêm");
+    var userInfo = UserInfo(
+        name: 'Tùng',
+        email: 'Tung@gmail.com',
+        phoneNumber: '0979712341',
+        birthDate: null,
+        address: addressInfo);
+    test('Create province objects', () {
+      expect(province.id, equals('01'));
+      expect(province.name, equals('Thành phố Hà Nội'));
+      expect(province.level, equals('Thành phố trung ương'));
+    });
+    test('Create district objects', () {
+      expect(district.id, equals('924'));
+      expect(district.provinceId, equals('92'));
+      expect(district.name, equals('Huyện Vĩnh Thạnh'));
+      expect(district.level, equals('Huyện'));
+    });
+    test('Create ward object', () {
+      expect(ward.id, equals('00001'));
+      expect(ward.provinceId, equals('01'));
+      expect(ward.districtId, equals('001'));
+      expect(ward.name, equals('Phường Phúc Xá'));
+      expect(ward.level, equals('Phường'));
+    });
+    test('Create addressInfo object', () {
+      expect(addressInfo.province?.name, equals('Thành phố Hà Nội'));
+      expect(addressInfo.province?.id, equals('01'));
+      expect(addressInfo.province?.level, equals('Thành phố trung ương'));
+      expect(addressInfo.district?.id, equals('924'));
+      expect(addressInfo.district?.provinceId, equals('92'));
+      expect(addressInfo.district?.name, equals('Huyện Vĩnh Thạnh'));
+      expect(addressInfo.district?.level, equals('Huyện'));
+      expect(addressInfo.ward?.id, equals('00001'));
+      expect(addressInfo.ward?.provinceId, equals('01'));
+      expect(addressInfo.ward?.districtId, equals('001'));
+      expect(addressInfo.ward?.name, equals('Phường Phúc Xá'));
+      expect(addressInfo.ward?.level, equals('Phường'));
+      expect(addressInfo.street, equals('Đường Bắc Từ Liêm'));
+    });
+    test('Create userInfo object', () {
+      expect(userInfo.name, equals("Tùng"));
+      expect(userInfo.email, equals("Tung@gmail.com"));
+      expect(userInfo.phoneNumber, equals("0979712341"));
+      expect(userInfo.birthDate, equals(null));
+      expect(userInfo.address, equals(addressInfo));
+    });
+  });
+  group('Test rightnest', () {
+    var provinceList;
+    var districtList;
+    var wardList;
+    test('If variable have data from json file', () {
+      Future<void> loadLocationData() async {
+        final file = File('test/resource/don_vi_hanh_chinh_test.json');
+        Map<String, dynamic> jsonData = jsonDecode(await file.readAsString());
+        List provinceData = jsonData["province"];
+        provinceList =
+            provinceData.map((json) => Province.fromMap(json)).toList();
+        expect(provinceList.hasData, true);
+        List districtData = jsonData["district"];
+        districtList =
+            districtData.map((json) => District.fromMap(json)).toList();
+        expect(districtList.hasData, true);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+        List wardData = jsonData["ward"];
+        wardList = wardData.map((json) => Ward.fromMap(json)).toList();
+        expect(wardList.hasData, true);
+      }
+    });
+    test('If list read right data from json file', () {
+      Future<void> loadLocationData() async {
+        final file = File('test/resource/don_vi_hanh_chinh_test.json');
+        Map<String, dynamic> jsonData = jsonDecode(await file.readAsString());
+        List provinceData = jsonData["province"];
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+        expect(provinceData[0]['id'], equals('01'));
+        expect(provinceData[0]['name'], equals('Thành phố Hà Nội'));
+        expect(provinceData[0]['level'], equals('Thành phố Trung ương'));
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+        List districtData = jsonData["district"];
+
+        expect(districtData[0]['id'], equals('001'));
+        expect(districtData[0]['name'], equals('Quận Ba Đình'));
+        expect(districtData[0]['level'], equals('Quận'));
+        expect(districtData[0]['provinceId'], equals('01'));
+
+        List wardData = jsonData["ward"];
+
+        expect(wardData[0]['id'], equals('00001'));
+        expect(wardData[0]['name'], equals('Phường Phúc Xá'));
+        expect(wardData[0]['level'], equals('Phường'));
+        expect(wardData[0]['provinceId'], equals('01'));
+        expect(wardData[0]['districtId'], equals('001'));
+      }
+    });
   });
 }
