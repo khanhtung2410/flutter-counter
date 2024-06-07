2bca3faca1e60a699e99b5e24a7cae0a2c3aca84
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2024-06-07 11:39:46 +0700

    chỉnh lại class transaction thành list để lưu nhiều giao dịch

--- money_manage/lib/data/userInfo.dart
+++ money_manage/lib/data/userInfo.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
 
 class UserInfo {
   String? name;
@@ -8,7 +7,7 @@ class UserInfo {
   String? totalBalance;
   String? inflow;
   String? outflow;
-  Transaction? transactions;
+  List<Transaction>? transactions;
   UserInfo({
     this.name,
     this.gender,
@@ -16,291 +15,88 @@ class UserInfo {
     this.totalBalance,
     this.inflow,
     this.outflow,
-    this.transactions,
-  });
-
-  UserInfo copyWith({
-    String? name,
-    String? gender,
-    String? birthday,
-    String? totalBalance,
-    String? inflow,
-    String? outflow,
-    Transaction? transactions,
-  }) {
-    return UserInfo(
-      name: name ?? this.name,
-      gender: gender ?? this.gender,
-      birthday: birthday ?? this.birthday,
-      totalBalance: totalBalance ?? this.totalBalance,
-      inflow: inflow ?? this.inflow,
-      outflow: outflow ?? this.outflow,
-      transactions: transactions ?? this.transactions,
-    );
-  }
-
+    List<Transaction>? transactions,
+  }) : transactions = transactions ?? [];
   Map<String, dynamic> toMap() {
-    return <String, dynamic>{
+    return {
       'name': name,
       'gender': gender,
       'birthday': birthday,
       'totalBalance': totalBalance,
       'inflow': inflow,
       'outflow': outflow,
-      'transactions': transactions?.toMap(),
+      'transactions':
+          transactions?.map((transaction) => transaction.toMap()).toList(),
     };
   }
 
-  factory UserInfo.fromMap(Map<String, dynamic> map) {
+  static UserInfo fromMap(Map<String, dynamic> map) {
     return UserInfo(
-      name: map['name'] != null ? map['name'] as String : null,
-      gender: map['gender'] != null ? map['gender'] as String : null,
-      birthday: map['birthday'] != null ? map['birthday'] as String : null,
-      totalBalance: map['totalBalance'] != null ? map['totalBalance'] as String : null,
-      inflow: map['inflow'] != null ? map['inflow'] as String : null,
-      outflow: map['outflow'] != null ? map['outflow'] as String : null,
-      transactions: map['transactions'] != null ? Transaction.fromMap(map['transactions'] as Map<String,dynamic>) : null,
+      name: map['name'],
+      gender: map['gender'],
+      birthday: map['birthday'],
+      totalBalance: map['totalBalance'],
+      inflow: map['inflow'],
+      outflow: map['outflow'],
+      transactions: (map['transactions'] as List<dynamic>?)
+              ?.map((transactionMap) => Transaction.fromMap(transactionMap))
+              .toList() ??
+          [],
     );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory UserInfo.fromJson(String source) =>
-      UserInfo.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() {
-    return 'UserInfo(name: $name, gender: $gender, birthday: $birthday, totalBalance: $totalBalance, inflow: $inflow, outflow: $outflow, transactions: $transactions)';
-  }
-
-  @override
-  bool operator ==(covariant UserInfo other) {
-    if (identical(this, other)) return true;
-  
-    return 
-      other.name == name &&
-      other.gender == gender &&
-      other.birthday == birthday &&
-      other.totalBalance == totalBalance &&
-      other.inflow == inflow &&
-      other.outflow == outflow &&
-      other.transactions == transactions;
-  }
-
-  @override
-  int get hashCode {
-    return name.hashCode ^
-        gender.hashCode ^
-        birthday.hashCode ^
-        totalBalance.hashCode ^
-        inflow.hashCode ^
-        outflow.hashCode ^
-        transactions.hashCode;
   }
 }
 
-class TransactionType {
-  String? transactionLabel;
-  String? transactionNumber;
-  TransactionType({
-    this.transactionLabel,
-    this.transactionNumber,
-  });
+List<String> transactionTypes = <String>['Inflow', 'Outflow'];
 
-  TransactionType copyWith({
-    String? transactionLabel,
-    String? transactionNumber,
-  }) {
-    return TransactionType(
-      transactionLabel: transactionLabel ?? this.transactionLabel,
-      transactionNumber: transactionNumber ?? this.transactionNumber,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'transactionLabel': transactionLabel,
-      'transactionNumber': transactionNumber,
-    };
-  }
-
-  factory TransactionType.fromMap(Map<String, dynamic> map) {
-    return TransactionType(
-      transactionLabel: map['transactionLabel'] != null ? map['transactionLabel'] as String : null,
-      transactionNumber: map['transactionNumber'] != null ? map['transactionNumber'] as String : null,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory TransactionType.fromJson(source) =>
-      TransactionType.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() =>
-      'TransactionType(transactionLabel: $transactionLabel, transactionNumber: $transactionNumber)';
-
-  @override
-  bool operator ==(covariant TransactionType other) {
-    if (identical(this, other)) return true;
-
-    return other.transactionLabel == transactionLabel &&
-        other.transactionNumber == transactionNumber;
-  }
-
-  @override
-  int get hashCode => transactionLabel.hashCode ^ transactionNumber.hashCode;
-}
-
-class ItemCategoryType {
-  String? categoryLabel;
-  String? categoryNumber;
-  ItemCategoryType({
-    this.categoryLabel,
-    this.categoryNumber,
-  });
-
-  ItemCategoryType copyWith({
-    String? categoryLabel,
-    String? categoryNumber,
-  }) {
-    return ItemCategoryType(
-      categoryLabel: categoryLabel ?? this.categoryLabel,
-      categoryNumber: categoryNumber ?? this.categoryNumber,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'categoryLabel': categoryLabel,
-      'categoryNumber': categoryNumber,
-    };
-  }
-
-  factory ItemCategoryType.fromMap(Map<String, dynamic> map) {
-    return ItemCategoryType(
-      categoryLabel: map['categoryLabel'] != null ? map['categoryLabel'] as String : null,
-      categoryNumber: map['categoryNumber'] != null ? map['categoryNumber'] as String : null,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory ItemCategoryType.fromJson(String source) => ItemCategoryType.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() =>
-      'ItemCategoryType(categoryLabel: $categoryLabel, categoryNumber: $categoryNumber)';
-
-  @override
-  bool operator ==(covariant ItemCategoryType other) {
-    if (identical(this, other)) return true;
-
-    return other.categoryLabel == categoryLabel &&
-        other.categoryNumber == categoryNumber;
-  }
-
-  @override
-  int get hashCode => categoryLabel.hashCode ^ categoryNumber.hashCode;
-}
+List<String> itemCategoryTypes = <String>[
+  'Cá nhân',
+  'Già đình',
+  'Quà',
+  'Đồ ăn'
+];
 
 class Transaction {
-  TransactionType? transactionType;
-  ItemCategoryType? categoryType;
-  String? itemCategoryName;
+  String? transactionType;
+  String? itemCategoryType;
   String? itemName;
   String? amount;
   String? date;
   Transaction({
     this.transactionType,
-    this.categoryType,
-    this.itemCategoryName,
+    this.itemCategoryType,
     this.itemName,
     this.amount,
     this.date,
   });
-
-  Transaction copyWith({
-    TransactionType? transactionType,
-    ItemCategoryType? categoryType,
-    String? itemCategoryName,
-    String? itemName,
-    String? amount,
-    String? date,
-  }) {
-    return Transaction(
-      transactionType: transactionType ?? this.transactionType,
-      categoryType: categoryType ?? this.categoryType,
-      itemCategoryName: itemCategoryName ?? this.itemCategoryName,
-      itemName: itemName ?? this.itemName,
-      amount: amount ?? this.amount,
-      date: date ?? this.date,
-    );
-  }
-
   Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'transactionType': transactionType?.toMap(),
-      'categoryType': categoryType?.toMap(),
-      'itemCategoryName': itemCategoryName,
+    return {
+      'transactionType': transactionType,
+      'itemCategoryType': itemCategoryType,
       'itemName': itemName,
       'amount': amount,
       'date': date,
     };
   }
 
-  factory Transaction.fromMap(Map<String, dynamic> map) {
+  static Transaction fromMap(Map<String, dynamic> map) {
     return Transaction(
-      transactionType: map['transactionType'] != null
-          ? TransactionType.fromMap(
-              map['transactionType'] as Map<String, dynamic>)
-          : null,
-      categoryType: map['categoryType'] != null
-          ? ItemCategoryType.fromMap(
-              map['categoryType'] as Map<String, dynamic>)
-          : null,
-      itemCategoryName: map['itemCategoryName'] != null
-          ? map['itemCategoryName'] as String
-          : null,
-      itemName: map['itemName'] != null ? map['itemName'] as String : null,
-      amount: map['amount'] != null ? map['amount'] as String : null,
-      date: map['date'] != null ? map['date'] as String : null,
+      transactionType: map['transactionType'],
+      itemCategoryType: map['itemCategoryType'],
+      itemName: map['itemName'],
+      amount: map['amount'],
+      date: map['date'],
     );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Transaction.fromJson(String source) =>
-      Transaction.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() {
-    return 'Transaction(transactionType: $transactionType,categoryType: $categoryType, itemCategoryName: $itemCategoryName, itemName: $itemName, amount: $amount, date: $date)';
-  }
-
-  @override
-  bool operator ==(covariant Transaction other) {
-    if (identical(this, other)) return true;
-
-    return other.transactionType == transactionType &&
-        other.categoryType == categoryType &&
-        other.itemCategoryName == itemCategoryName &&
-        other.itemName == itemName &&
-        other.amount == amount &&
-        other.date == date;
-  }
-
-  @override
-  int get hashCode {
-    return transactionType.hashCode ^
-        categoryType.hashCode ^
-        itemCategoryName.hashCode ^
-        itemName.hashCode ^
-        amount.hashCode ^
-        date.hashCode;
   }
 }
 
-List<Transaction> transactions1 = [Transaction()];
-
-UserInfo userdata = UserInfo();
+UserInfo getMockUserInfo() {
+  return UserInfo(
+    name: 'John Doe',
+    gender: 'Male',
+    birthday: '1990-01-01',
+    totalBalance: '1000',
+    inflow: '500',
+    outflow: '300',
+    transactions: null, // No transactions for testing
+  );
+}
