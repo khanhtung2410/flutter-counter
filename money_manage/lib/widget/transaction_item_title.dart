9b0464ca8dcc6b4339b892fa1ec22ffe39955bcc
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2024-06-07 11:43:13 +0700

    chuyển thành hiện một list giao dịch

--- money_manage/lib/widget/transaction_item_title.dart
+++ money_manage/lib/widget/transaction_item_title.dart
@@ -5,18 +5,12 @@ import 'package:money_manage/data/userInfo.dart';
 import 'package:money_manage/ultils/colors_and_size.dart';
 
 class TransactionItemTile extends StatelessWidget {
-  final Transaction transaction;
-  const TransactionItemTile({Key? key, required this.transaction})
-      : super(key: key);
+  final List<Transaction> transactions;
 
-  // String getSign(String? type) {
-  //   switch (type) {
-  //     case "2":
-  //       return "+";
-  //     case "1":
-  //       return "-";
-  //   }
-  // }
+  const TransactionItemTile({
+    Key? key,
+    required this.transactions,
+  }) : super(key: key);
 
   Color getRandomBgColor() {
     return Color(Random().nextInt(0XFF000000));
@@ -24,58 +18,73 @@ class TransactionItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.symmetric(vertical: defaultSpacing / 2),
-        decoration: const BoxDecoration(
+    return Column(
+      children: transactions.map((transaction) {
+        return Container(
+          margin: const EdgeInsets.symmetric(vertical: defaultSpacing / 2),
+          decoration: const BoxDecoration(
             boxShadow: [
               BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset.zero,
-                  blurRadius: 10,
-                  spreadRadius: 4)
+                color: Colors.black12,
+                offset: Offset.zero,
+                blurRadius: 10,
+                spreadRadius: 4,
+              ),
             ],
             color: background,
-            borderRadius: BorderRadius.all(Radius.circular(defaultRadius))),
-        child: ListTile(
-          leading: Container(
+            borderRadius: BorderRadius.all(Radius.circular(defaultRadius)),
+          ),
+          child: ListTile(
+            leading: Container(
               padding: const EdgeInsets.all(defaultSpacing / 2),
               decoration: BoxDecoration(
-                  color: getRandomBgColor(),
-                  borderRadius: const BorderRadius.all(
-                      Radius.circular(defaultRadius / 2))),
-              child: 1 == 1
-                  ? const Icon(Icons.supervised_user_circle_sharp)
-                  : const Icon(Icons.house)),
-          title: Text(transaction.itemCategoryName ?? "",
+                color: getRandomBgColor(),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(defaultRadius / 2),
+                ),
+              ),
+              child: const Icon(Icons.supervised_user_circle_sharp),
+            ),
+            title: Text(
+              transaction.itemCategoryType ?? "No category", // Handle null case
               style: Theme.of(context).textTheme.bodySmall?.copyWith(
                     color: fontHeading,
                     fontSize: fontSizeTitle,
                     fontWeight: FontWeight.w700,
-                  )),
-          subtitle: Text(transaction.itemName ?? "",
+                  ),
+            ),
+            subtitle: Text(
+              transaction.itemName ?? "No name", // Handle null case
               style: Theme.of(context).textTheme.bodySmall?.copyWith(
                     color: fontSubheading,
                     fontSize: fontSizeBody,
-                  )),
-          trailing: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                  // "${getSign(transaction.transactionType?.transactionNumber)} ${transaction.amount}
-                  " vnđ",
+                  ),
+            ),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "${transaction.transactionType ?? ""} ${transaction.amount ?? ""} vnđ", // Handle null cases
                   style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                      color: transaction.transactionType?.transactionNumber == 2
-                          ? Colors.red
-                          : fontHeading,
-                      fontSize: fontSizeTitle)),
-              Text(transaction.date ?? "",
+                        color: transaction.transactionType == '2'
+                            ? Colors.red
+                            : fontHeading,
+                        fontSize: fontSizeTitle,
+                      ),
+                ),
+                Text(
+                  transaction.date ?? "No date", // Handle null case
                   style: Theme.of(context).textTheme.bodySmall?.copyWith(
                         color: fontSubheading,
                         fontSize: fontSizeBody,
-                      ))
-            ],
+                      ),
+                ),
+              ],
+            ),
           ),
-        ));
+        );
+      }).toList(),
+    );
   }
 }
