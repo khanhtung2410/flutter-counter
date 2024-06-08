40ad57b1c977a43fe764218b599728e432fcf372
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2024-06-08 15:46:03 +0700

    chỉnh sửa lại để hiện giao dịch

--- money_manage/lib/widget/transaction_item_title.dart
+++ money_manage/lib/widget/transaction_item_title.dart
@@ -5,86 +5,93 @@ import 'package:money_manage/data/userInfo.dart';
 import 'package:money_manage/ultils/colors_and_size.dart';
 
 class TransactionItemTile extends StatelessWidget {
-  final List<Transaction> transactions;
-
+  final Transaction? transaction;
   const TransactionItemTile({
     Key? key,
-    required this.transactions,
+    required this.transaction,
   }) : super(key: key);
 
   Color getRandomBgColor() {
     return Color(Random().nextInt(0XFF000000));
   }
 
+  String getSign(String? a) {
+    switch (a) {
+      case "Outflow":
+        return "-";
+      case "Inflow":
+        return "+";
+    }
+    return "";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((transaction) {
-        return Container(
-          margin: const EdgeInsets.symmetric(vertical: defaultSpacing / 2),
-          decoration: const BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black12,
-                offset: Offset.zero,
-                blurRadius: 10,
-                spreadRadius: 4,
+    return Column(children: [
+      Container(
+        margin: const EdgeInsets.symmetric(vertical: defaultSpacing / 2),
+        decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset.zero,
+              blurRadius: 10,
+              spreadRadius: 4,
+            ),
+          ],
+          color: background,
+          borderRadius: BorderRadius.all(Radius.circular(defaultRadius)),
+        ),
+        child: ListTile(
+          leading: Container(
+            padding: const EdgeInsets.all(defaultSpacing / 2),
+            decoration: BoxDecoration(
+              color: getRandomBgColor(),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(defaultRadius / 2),
+              ),
+            ),
+            child: const Icon(Icons.supervised_user_circle_sharp),
+          ),
+          title: Text(
+            transaction?.itemCategoryType ?? "No category", // Handle null case
+            style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                  color: fontHeading,
+                  fontSize: fontSizeTitle,
+                  fontWeight: FontWeight.w700,
+                ),
+          ),
+          subtitle: Text(
+            transaction?.itemName ?? "No name", // Handle null case
+            style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                  color: fontSubheading,
+                  fontSize: fontSizeBody,
+                ),
+          ),
+          trailing: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                "${getSign(transaction?.transactionType)} ${transaction?.amount ?? ""} vnđ", // Handle null cases
+                style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      color: transaction?.transactionType == 'Outflow'
+                          ? Colors.red
+                          : fontHeading,
+                      fontSize: fontSizeTitle,
+                    ),
+              ),
+              Text(
+                transaction?.date ?? "No date", // Handle null case
+                style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      color: fontSubheading,
+                      fontSize: fontSizeBody,
+                    ),
               ),
             ],
-            color: background,
-            borderRadius: BorderRadius.all(Radius.circular(defaultRadius)),
           ),
-          child: ListTile(
-            leading: Container(
-              padding: const EdgeInsets.all(defaultSpacing / 2),
-              decoration: BoxDecoration(
-                color: getRandomBgColor(),
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(defaultRadius / 2),
-                ),
-              ),
-              child: const Icon(Icons.supervised_user_circle_sharp),
-            ),
-            title: Text(
-              transaction.itemCategoryType ?? "No category", // Handle null case
-              style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                    color: fontHeading,
-                    fontSize: fontSizeTitle,
-                    fontWeight: FontWeight.w700,
-                  ),
-            ),
-            subtitle: Text(
-              transaction.itemName ?? "No name", // Handle null case
-              style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                    color: fontSubheading,
-                    fontSize: fontSizeBody,
-                  ),
-            ),
-            trailing: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  "${transaction.transactionType ?? ""} ${transaction.amount ?? ""} vnđ", // Handle null cases
-                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                        color: transaction.transactionType == '2'
-                            ? Colors.red
-                            : fontHeading,
-                        fontSize: fontSizeTitle,
-                      ),
-                ),
-                Text(
-                  transaction.date ?? "No date", // Handle null case
-                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                        color: fontSubheading,
-                        fontSize: fontSizeBody,
-                      ),
-                ),
-              ],
-            ),
-          ),
-        );
-      }).toList(),
-    );
+        ),
+      ),
+    ]);
   }
 }
