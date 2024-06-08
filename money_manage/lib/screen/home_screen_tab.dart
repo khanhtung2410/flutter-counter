7bd8f6ee1bbbeae8dfe90c080a10bef4578bf8e4
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2024-06-09 03:27:30 +0700

    Lấy dữ liệu từ localstore được, thêm phần cập nhật danh sách và dòng tiền khi tạo giao dịch

--- money_manage/lib/screen/home_screen_tab.dart
+++ money_manage/lib/screen/home_screen_tab.dart
@@ -19,17 +19,21 @@ class HomeScreenTab extends StatefulWidget {
 class _HomeScreenTabState extends State<HomeScreenTab> {
   UserInfo userInfo = getMockUserInfo();
   bool isLoaded = false;
-
+  //Tạo dữ liệu và gọi function lấy dữ liệu từ local store
   @override
   void initState() {
     super.initState();
+    //Gọi funtion lấy dữ liệu
     _loadUserInfo();
   }
-
+  //Funtion lấy dữ liệu và kiểm tra xem dữ liệu có null không
   Future<void> _loadUserInfo() async {
+    //Đợi lấy dữ liệu
     final userData = await LocalStorageManager.loadUserInfo();
+    //Kiểm tra xem dữ liêu null không
     if (userData != null) {
       setState(() {
+        //Nếu dữ liệu không null thì userInfo sẽ là dữ liệu từ local store
         userInfo = userData;
         isLoaded = true;
       });
@@ -38,6 +42,7 @@ class _HomeScreenTabState extends State<HomeScreenTab> {
 
   @override
   Widget build(BuildContext context) {
+    //Dùng Consumer để chỉnh UI khi có thay đổi liên quan đến UserInfo
     return Consumer<UserInfo>(
       builder: (context, value, child) {
         return SingleChildScrollView(
@@ -47,6 +52,7 @@ class _HomeScreenTabState extends State<HomeScreenTab> {
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               const SizedBox(height: defaultSpacing * 2),
               ListTile(
+                //Lấy tên người dùng từ dữ liêu được lưu
                 title: Text('Xin chào! ${userInfo.name ?? ""}'),
                 leading: Image.asset("assest/picture/like.png"),
                 trailing: Image.asset('assest/icon/bell-icon.png'),
@@ -64,7 +70,8 @@ class _HomeScreenTabState extends State<HomeScreenTab> {
                     ),
                     const SizedBox(height: defaultSpacing / 2),
                     Text(
-                      "${value.totalBalance ?? "0"} vnđ",
+                      //Lấy dữ liệu tổng tiền từ local store và thay đổi nó khi nhập giao dịch mới
+                      "${((int.tryParse(userInfo.totalBalance ?? "0") ?? 0) + (int.tryParse(value.totalBalance ?? "0") ?? 0)).toString()} vnđ",
                       style: Theme.of(context)
                           .textTheme
                           .titleLarge
@@ -74,14 +81,16 @@ class _HomeScreenTabState extends State<HomeScreenTab> {
                 ),
               ),
               const SizedBox(height: defaultSpacing * 2),
-               Row(
+              Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Expanded(
+                    //Lớp tự làm để trình bày dòng tiền người dùng
                     child: IncomeExpenseCard(
                       expenseData: ExpenseData(
                         'Thu nhập',
-                        "${value.inflow ?? "0"} vnđ",
+                        //Lấy dữ liệu dòng tiền thu từ local store và thay đổi nó khi nhập giao dịch mới
+                        "${((int.tryParse(userInfo.inflow ?? "0") ?? 0) + (int.tryParse(value.inflow ?? "0") ?? 0) / 2).toString()} vnđ",
                         Icons.arrow_upward_rounded,
                       ),
                     ),
@@ -91,7 +100,8 @@ class _HomeScreenTabState extends State<HomeScreenTab> {
                     child: IncomeExpenseCard(
                       expenseData: ExpenseData(
                         'Chi tiêu',
-                        "${value.outflow ?? "0"} vnđ",
+                        //Lấy dữ liệu dòng tiền chi từ local store và thay đổi nó khi nhập giao dịch mới
+                        "${((int.tryParse(userInfo.outflow ?? "0") ?? 0) + (int.tryParse(value.outflow ?? "0") ?? 0) / 2).toString()} vnđ",
                         Icons.arrow_downward_rounded,
                       ),
                     ),
@@ -107,17 +117,54 @@ class _HomeScreenTabState extends State<HomeScreenTab> {
                     ?.copyWith(fontWeight: FontWeight.w700),
               ),
               const SizedBox(height: defaultSpacing),
-              Text(
-                "{userInfo.transactions}",
-                style: TextStyle(color: fontSubheading),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Mới thêm vào',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(fontWeight: FontWeight.w500),
+                  ),
+                  //Tạo view một danh sách những giao dịch mới thêm trong lần sử dụng này
+                  ListView.builder(
+                      physics: AlwaysScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      //Lấy dữ liệu số giao dịch từ Consumer
+                      itemCount: value.transactions?.length ?? 0,
+                      itemBuilder: (context, index) {
+                        //Gán từng thành phần của danh sách trong Consumer vào lớp Transaction
+                        Transaction? transaction = value.transactions?[index];
+                        //Trả về lớp được xây để chiếu giao dịch
+                        return TransactionItemTile(transaction: transaction);
+                      }),
+                ],
               ),
-              ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: value.transactions?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    Transaction? transaction = value.transactions?[index];
-                    return TransactionItemTile(transaction: transaction);
-                  }),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Giao dịch cũ',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(fontWeight: FontWeight.w500),
+                  ),
+                  //Tạo view danh sách lấy từ local store (giao dịch mới thêm chỉ vào đây khi khởi động lại ứng dụng)
+                  ListView.builder(
+                      physics: AlwaysScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      //Lấy độ dài danh sách giao dịch từ local store
+                      itemCount: userInfo.transactions?.length ?? 0,
+                      itemBuilder: (context, index) {
+                        //Gán thành phần từ danh sách local store
+                        Transaction? transaction =
+                            userInfo.transactions?[index];
+                        return TransactionItemTile(transaction: transaction);
+                      }),
+                ],
+              )
             ]),
           ),
         );
